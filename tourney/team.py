62b059ad6f2a63d65a3080c1536cae4d55d16cfd
dakotamurdock <dakota.murdock@outlook.com>
2024-05-01 06:36:28 -0400

    adding scoring to team

--- tourney/team.py
+++ tourney/team.py
@@ -1,6 +1,8 @@
 from typing import Dict, List, Optional, Union
 from uuid import uuid1
 
+import pandas as pd
+
 from tourney.person import Person
 
 
@@ -8,32 +10,54 @@ class Group:
     def __init__(
         self,
         name: str,
-        roster: Union[List[Person], Dict[str, Person]]
+        roster: Union[List[Person], Dict[str, Person]],
+        id: str = str(uuid1())
     ):
-        self.uuid = str(uuid1())
+        self.id = id
         self.name = name
-        self.roster = roster
+
+        self.roster: pd.DataFrame = None
+        if roster:
+            if isinstance(roster[0], str):
+                self.roster = pd.DataFrame(data=roster, columns=["person"])
+            elif isinstance(roster[0], dict):
+                self.roster = pd.DataFrame(data=roster, columns=["division", "person"])
+        else:
+            raise AttributeError("A roster must be provided to build a group.")
+        self.roster["group"] = self
 
 
 class Team:
     def __init__(
         self,
         name: str,
-        roster: Union[List[Person], Dict[str, Person]],
+        id: str = str(uuid1()),
+        roster: Union[List[Person], Dict[str, Person]] = None,
         groups: Optional[List[Group]] = None,
         location: Optional[str] = None,
         coaches: Optional[Union[List[Person], Dict[str, Person]]] = None
     ):
-        self.uuid = str(uuid1())
+        self.id = id
         self.name = name
         self.groups = groups
-
-        if self.groups:
-            self.roster = {}
-            for group in self.groups:
-                self.roster[group] = group.roster
-        else:
-            self.roster = roster
-
         self.location = location
         self.coaches = coaches
+
+        self.roster: pd.DataFrame = None
+        if roster:
+            if isinstance(roster[0], str):
+                self.roster = pd.DataFrame(data=roster, columns=["person"])
+            elif isinstance(roster[0], dict):
+                self.roster = pd.DataFrame(data=roster, columns=["division", "person"])
+        else:
+            if self.groups:
+                self.roster = pd.concat([group.roster for group in self.groups])
+            else:
+                raise AttributeError("Either a roster or a list of groups with rosters must be provided to build a team.")
+        self.roster["team"] = self
+
+        self.box_score = self.roster.copy()
+        self.box_score["score"] = None
+    
+    def consolidate_score(self):
+        scores = self.box_score.groupby([])["score"]
--- tourney/tests/test_objects.py
+++ tourney/tests/test_objects.py
@@ -2,110 +2,200 @@ import random
 import string
 
 import pandas as pd
+import numpy as np
 from faker import Faker
 
 from tourney.competition import Competition
 from tourney.event import Event
 from tourney.person import Person
 from tourney.scoring import sum_scores
-from tourney.team import Team
+from tourney.team import Team, Group
 
+# Set print configs
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
 # Create a Faker instance
 fake = Faker()
 
 
-def test_create_team():
+def test_person():
+    """Test instantiation of Person class and attribute consistency"""
+    test_name = fake.name()
+    test_birthdate = fake.date()
+    test_gender = random.choice(["male", "female", "transgender"])
+    test_height = np.random.randint(low=60, high=79)
+    test_weight = np.random.randint(low=105, high=247)
 
-    test_name = fake.company()
-    test_roster = {random.choice(string.ascii_letters): Person(name=fake.name()) for _ in range(15)}
-    test_coaches = {random.choice(string.ascii_letters): fake.name() for _ in range(3)}
-    test_location = fake.city()
-
-    test_team = Team(
+    test_person = Person(
         name=test_name,
-        roster=test_roster,
-        location=test_location,
-        coaches=test_coaches
+        birthdate=test_birthdate,
+        gender=test_gender,
+        height=test_height,
+        weight=test_weight
     )
 
-    # test object attributes
-    assert test_team.name == test_name
-    assert test_team.roster == test_roster
-    assert test_team.location == test_location
-    assert test_team.coaches == test_coaches
+    assert isinstance(test_person, Person)
+    assert test_person.name == test_name
+    assert test_person.birthdate == test_birthdate
+    assert test_person.gender == test_gender
+    assert test_person.height == test_height
+    assert test_person.weight == test_weight
 
 
-def test_create_competition_w_two_halves():
-    # Create Teams that will compete
-    team_a = Team(
-        name="A Team",
-        roster=[Person(name=fake.name()) for _ in range(10)],
-    )
-    team_b = Team(
-        name="B Team",
-        roster=[Person(name=fake.name()) for _ in range(10)],
-    )
-    teams = [team_a, team_b]
+def test_group():
+    divisions = ["A", "B"]
+    test_roster = [{"division": random.choice(divisions), "person": Person(name=fake.name())} for _ in range(20)]
 
-    # Set up Events of the competition
-    start_time = fake.date_time(),
-    first_half = Event(
-        name="1st Half",
-        participants=[team_a, team_b],
-        team_scoring_function=sum_scores,
-        person_level_scoring=True,
-        duration=30,
-        duration_unit="min",
-        scoring_unit="points"
+    test_name_a = fake.company()
+    test_name_b = fake.company()
+    test_name_c = fake.company()
+    test_name_d = fake.company()
+ 
+    group_a = Group(
+        name=test_name_a,
+        roster=test_roster[0:5]
     )
-    second_half = Event(
-        name="2nd Half",
-        participants=[team_a, team_b],
-        duration=30,
-        duration_unit="min",
-        scoring_unit="points"
+    group_b = Group(
+        name=test_name_b,
+        roster=test_roster[5:10]
+    )
+    group_c = Group(
+        name=test_name_c,
+        roster=test_roster[10:15]
+    )
+    group_d = Group(
+        name=test_name_d,
+        roster=test_roster[15:20]
     )
 
-    assert isinstance(first_half.scoring_table, pd.DataFrame)
+    assert group_a.name == test_name_a
+    assert group_b.name == test_name_b
+    assert group_c.name == test_name_c
+    assert group_d.name == test_name_d
 
-    score_sim = []
-    for team in teams:
-        for person in team.roster:
-            score_sim.append(
-                {"team": team.uuid, "person": person.uuid, "score": random.randint(0, 15)}
-            )
+    assert group_a.roster.shape == (5, 3)
+    assert group_b.roster.shape == (5, 3)
+    assert group_c.roster.shape == (5, 3)
+    assert group_d.roster.shape == (5, 3)
 
-    first_half.update_scores(new_scores=score_sim)
-    print(first_half.get_scores())
-    raise AssertionError
-    # second_half.update_scores(new_scores={team_a: 26, team_b: 23})
+    test_team_name_a = fake.company()
+    test_team_name_b = fake.company()
 
-    # for test_cumulative_scoring_method in IterableCumulativeScoringMethods:
-    #     # Set up Competition
-    #     game = Competition(
-    #         name="Game 1",
-    #         cumulative_scoring_method=test_cumulative_scoring_method,
-    #         datetime_start=start_time,
-    #         events=[first_half, second_half],
-    #         event_graph={
-    #             first_half: [second_half]
-    #         }
-    #     )
+    test_location_a = fake.city()
+    test_location_b = fake.city()
 
-    #     assert [participant.name for participant in game.participants].sort() == ["A Team", "B Team"].sort()
-    #     final_score = game.get_score()
+    test_team_a = Team(
+        name=test_team_name_a,
+        groups=[group_a, group_c],
+        location=test_location_a
+    )
 
-    #     if test_cumulative_scoring_method == "sum":
-    #         assert final_score[team_a] == 48
-    #         assert final_score[team_b] == 47
-    #     elif test_cumulative_scoring_method == "mean":
-    #         assert final_score[team_a] == 24
-    #         assert final_score[team_b] == 23.5
-    #     elif test_cumulative_scoring_method == "max":
-    #         assert final_score[team_a] == 26
-    #         assert final_score[team_b] == 24
-    #     elif test_cumulative_scoring_method == "min":
-    #         assert final_score[team_a] == 22
-    #         assert final_score[team_b] == 23
+    test_team_b = Team(
+        name=test_team_name_b,
+        groups=[group_b, group_d],
+        location=test_location_b
+    )
+
+    print(test_team_a.roster)
+    print(test_team_a.box_score)
+
+    raise AttributeError
+
+
+
+
+# def test_create_team():
+
+#     test_name = fake.company()
+#     test_roster = {random.choice(string.ascii_letters): Person(name=fake.name()) for _ in range(15)}
+#     test_coaches = {random.choice(string.ascii_letters): fake.name() for _ in range(3)}
+#     test_location = fake.city()
+
+#     test_team = Team(
+#         name=test_name,
+#         roster=test_roster,
+#         location=test_location,
+#         coaches=test_coaches
+#     )
+
+#     # test object attributes
+#     assert test_team.name == test_name
+#     assert test_team.roster == test_roster
+#     assert test_team.location == test_location
+#     assert test_team.coaches == test_coaches
+
+
+# def test_create_competition_w_two_halves():
+#     # Create Teams that will compete
+#     team_a = Team(
+#         name="A Team",
+#         roster=[Person(name=fake.name()) for _ in range(10)],
+#     )
+#     team_b = Team(
+#         name="B Team",
+#         roster=[Person(name=fake.name()) for _ in range(10)],
+#     )
+#     teams = [team_a, team_b]
+
+#     # Set up Events of the competition
+#     start_time = fake.date_time(),
+#     first_half = Event(
+#         name="1st Half",
+#         participants=[team_a, team_b],
+#         team_scoring_function=sum_scores,
+#         person_level_scoring=True,
+#         duration=30,
+#         duration_unit="min",
+#         scoring_unit="points"
+#     )
+#     second_half = Event(
+#         name="2nd Half",
+#         participants=[team_a, team_b],
+#         duration=30,
+#         duration_unit="min",
+#         scoring_unit="points"
+#     )
+
+#     assert isinstance(first_half.scoring_table, pd.DataFrame)
+
+#     score_sim = []
+#     for team in teams:
+#         for person in team.roster:
+#             score_sim.append(
+#                 {"team": team.uuid, "person": person.uuid, "score": random.randint(0, 15)}
+#             )
+
+#     first_half.update_scores(new_scores=score_sim)
+#     print(first_half.get_scores())
+#     raise AssertionError
+#     # second_half.update_scores(new_scores={team_a: 26, team_b: 23})
+
+#     # for test_cumulative_scoring_method in IterableCumulativeScoringMethods:
+#     #     # Set up Competition
+#     #     game = Competition(
+#     #         name="Game 1",
+#     #         cumulative_scoring_method=test_cumulative_scoring_method,
+#     #         datetime_start=start_time,
+#     #         events=[first_half, second_half],
+#     #         event_graph={
+#     #             first_half: [second_half]
+#     #         }
+#     #     )
+
+#     #     assert [participant.name for participant in game.participants].sort() == ["A Team", "B Team"].sort()
+#     #     final_score = game.get_score()
+
+#     #     if test_cumulative_scoring_method == "sum":
+#     #         assert final_score[team_a] == 48
+#     #         assert final_score[team_b] == 47
+#     #     elif test_cumulative_scoring_method == "mean":
+#     #         assert final_score[team_a] == 24
+#     #         assert final_score[team_b] == 23.5
+#     #     elif test_cumulative_scoring_method == "max":
+#     #         assert final_score[team_a] == 26
+#     #         assert final_score[team_b] == 24
+#     #     elif test_cumulative_scoring_method == "min":
+#     #         assert final_score[team_a] == 22
+#     #         assert final_score[team_b] == 23
 
